1c18370d68f27be9a6dda4ea116cd16440674311
Rebecca Newton <rebecca.newton@valtech.com>
2022-09-14 13:26:40 +0200

    css module for my buisness page

---
+++ pages/myBuisness.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from "next/head";
+import styles from "../styles/MyBuisness.css"
+
+const myBuisness: NextPage = () => {
+  return (
+    <>
+     <Head>
+        <title>myBuisness</title>
+        <meta name="keywords" content="error" />
+      </Head>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        <h1>Hello</h1>
+      myBuisness
+        </h1>
+      </main>
+    </div></>
+  )
+}
+
+export default myBuisness
